00b9e6b11b33d337ee6c120c8db58e37c98c5bbf
WildEyes <xwildeyes@gmail.com>
2013-12-04 19:56:07 +0200

    file organizations

--- app/js/first.js
+++ app/js/first.js
@@ -5,7 +5,6 @@ var useBooster = function(){
     return (MY_CONFIG.config.booster_enabled && MY_CONFIG.config.with_booster && document.URL.indexOf('#newtab') === -1 && document.URL.indexOf('background') === -1);
 };
 
-
 MY_CONFIG.init(function(config){
     if(useBooster()){
         window.open("newtab.html#newtab");
