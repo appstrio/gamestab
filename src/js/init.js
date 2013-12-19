0473768e4a756eb2c39360876a74011c269abe3b
Hadar Landao <hlandao@gmail.com>
2013-12-19 16:06:44 +0200

    -clean a few things at int.’s

--- src/js/init.js
+++ src/js/init.js
@@ -4,20 +4,21 @@
         'when', 'jquery', 'jfeed', 'moment',
         'underscore', 'uri', 'typeahead'
     ],
-        modules = [
-            'Config',
-            'Runtime',
-            'Renderer', 'SearchRenderer', 'MenuRenderer', 'DialsRenderer',
-            'Search',
-            'Storage',
-            'Provider', 'TopsitesProvider', 'AppsProvider', 'WebappsProvider',
-            'Dial', 'AppDial',
-        ],
-        dynamicPaths = {
-            env: 'env',
-            main: 'main',
-            templates: 'templates'
-        };
+    modules = [
+        'Config',
+        'Runtime',
+        'Renderer', 'SearchRenderer', 'MenuRenderer', 'DialsRenderer',
+        'Search',
+        'Storage',
+        'Provider', 'TopsitesProvider', 'AppsProvider', 'WebappsProvider',
+        'Dial', 'AppDial'
+    ],
+    dynamicPaths = {
+        env: 'env',
+        main: 'main',
+        templates: 'templates'
+    };
+
 
     while (modules.length) {
         var currentModule = modules.pop();
@@ -27,43 +28,45 @@
         var currentLib = libs.pop();
         dynamicPaths[currentLib] = 'libs/' + currentLib;
     }
-    //log(dynamicPaths);
+
+
     require.config({
         baseUrl: 'js/',
         paths: dynamicPaths,
         shim: {
             underscore: {
                 exports: '_'
-            },
+            }
         }
     });
 })();
+
+
 window.rErrReport = function requireJSErrorReport(err) {
     log(err);
 };
 
-define(function initWINT(require) {
 
+
+define(function initWINT(require) {
     //Using require to lazy-load main only after booster.
 
     var env = require('env'),
-        config = require('Config'),
-        runtime = require('Runtime');
+        config = require('Config');
 
     if (env.DEBUG && env.logLoadOrder) console.log("Loading Module : initWINT");
 
-    runtime.promise.then(function(runtimeData) {
+    config.promise.then(function(configData) {
         ///Check if runtime exists (= Not first run) and check whether to use the "booster"
-        var runtimeDataExists   = runtimeData,
-            useBooster          = runtimeData.useBooster,
+        var useBooster          = configData && configData.runtime && configData.runtime.useBooster,
             BrandNewPage        = document.URL.indexOf('#newtab') === -1,
             NotOnBackgroundPage = document.URL.indexOf('background') === -1;
 
 
-        if (runtimeDataExists && useBooster && BrandNewPage && NotOnBackgroundPage) {
-            alert('STAHP');
+        if (useBooster && BrandNewPage && NotOnBackgroundPage) {
             //Close & Open tab to move focus to the "main input"
-            window.open("newtab.html#newtab"); // TODO: consider to use the chrome api to improve the speed of the new window opening
+            chrome.tabs.create({url : "newtab.html#newtab"});
+            //window.open("newtab.html#newtab"); // TODO: consider to use the chrome api to improve the speed of the new window opening
             window.close();
         } else {
             require(['main'], null);
