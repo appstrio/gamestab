76300c24003917e46179ccd1f5f4fcc4026a87d4
Hadar Landao <hlandao@gmail.com>
2014-01-05 22:50:20 +0200

    - added fontello - created hl-launcher directive - added moving between screens in the directive

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,84 @@ var app = angular.module('myApp', ['ui.sortable']);
 angular.element(document).ready(function() {
     angular.bootstrap(document, ['myApp']);
 });
+
+app.directive('hlLauncher', [function(){
+    return function(scope, element){
+
+        scope.curScreen = 0;
+
+        var $viewport = element.find('.viewport').eq(0);
+        var $arrowLeft = element.find('.icon-left-open-big').eq(0);
+        var $arrowRight = element.find('.icon-right-open-big').eq(0);
+        var screenWidth = 900;
+
+        var getScreenWidth = function(numberOfScreen){
+            return screenWidth * numberOfScreen + 'px';
+        };
+
+        var getScreenPosition = function(curScreen){
+            return screenWidth * curScreen * (-1) + 'px';
+        };
+
+        var checkArrows = function(){
+            if(scope.curScreen > 0){
+                $arrowLeft.show();
+            }else{
+                $arrowLeft.hide();
+            }
+
+            if(scope.curScreen < scope.rawScreens.length - 1){
+                $arrowRight.show();
+            }else{
+                $arrowRight.hide();
+            }
+        }
+
+
+        $arrowLeft.click(function(){
+            scope.$apply(function(){
+                if(scope.curScreen > 0){
+                    --scope.curScreen;
+                }else if (scope.curScreen === 0) {
+
+                }else{
+
+                }
+            });
+        });
+
+
+        $arrowRight.click(function(){
+            scope.$apply(function(){
+                if(scope.curScreen < scope.rawScreens.length - 1){
+                    ++scope.curScreen;
+                }else if(scope.curScreen === scope.rawScreens.length){
+
+                }else{
+
+                }
+            });
+
+        });
+
+
+        scope.$watch('curScreen', function(newVal){
+            newVal = newVal || 0;
+            $viewport.css({left : getScreenPosition(newVal)});
+            checkArrows();
+        });
+
+
+        scope.$watch('rawScreens', function(newVal){
+            if(newVal && newVal.length)
+                $viewport.css({width : getScreenWidth(newVal.length)});
+            checkArrows();
+        });
+
+
+
+
+    }
+}]);
+
+
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -62,9 +62,10 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 
     };
 
-    $http.get('./data/defaultDials.json').success(function(responseDials){
-        console.log('responseDials',responseDials);
-        $scope.rawScreens = [responseDials];
+    $http.get('./data/defaultDials.json').success(function(responseApps){
+        $http.get('./data/games.json').success(function(responseGames){
+            $scope.rawScreens = [responseApps,responseGames.slice(0,15)];
+        });
     });
 
 //    $scope.rawScreens = [
