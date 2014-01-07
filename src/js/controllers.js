b404c65a0627ca9405a0262cf25d35406d393f2a
Hadar Landao <hlandao@gmail.com>
2014-01-07 12:20:49 +0200

    Bigger grid, working well.

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,7 +1,7 @@
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
     $http.get('./data/defaultDials.json').success(function(responseApps){
         $http.get('./data/games.json').success(function(responseGames){
-            $scope.rawScreens = [responseApps,responseGames.slice(0,15)];
+            $scope.rawScreens = [responseApps.slice(0,12),responseGames.slice(0,12)];
         });
     });
 }]);
