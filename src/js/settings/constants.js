89ad44778df37d082e579dea487428b3d1616eb7
Gilad Peleg <giladp007@gmail.com>
2014-02-05 23:41:07 +0200

    fixed ng-style in newtab.jade convert        gamestab json to partner.json

--- src/js/settings/constants.js
+++ src/js/settings/constants.js
@@ -3,7 +3,7 @@ var settingsModule = settingsModule || angular.module('aio.settings', []);
 settingsModule.factory('Constants', [
     function() {
         return {
-            PARTNERS_JSON_URL: '/data/gamestab_remote.json',
+            PARTNERS_JSON_URL: '/data/partners.json',
             STORAGE_KEYS: {
                 CONFIG: 'gt.config',
                 ACCOUNT: 'gt.account'
