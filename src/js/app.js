6687f96cf599b708b6788535cd9859f94bdbbe13
Gilad Peleg <giladp007@gmail.com>
2014-02-19 18:15:32 +0200

    minor

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,7 @@ angular.module('myApp', ['aio.main', 'aio.settings', 'ui.sortable',
     'aio.common', 'aio.launcher', 'aio.search', 'fallback.src', 'ngProgress'
 ]);
 
+//use cross domain requests
 angular.module('myApp').config(['$httpProvider',
     function ($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
