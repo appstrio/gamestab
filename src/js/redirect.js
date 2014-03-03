b04be002c04b4d5c1f80c8b339671ed4a72a2042
Gilad Peleg <giladp007@gmail.com>
2014-03-03 17:38:22 +0200

    redirect dev version

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,4 +1,4 @@
 //just redirect to target site
-if (document.location.href !== "http://s3.amazonaws.com/Gamestab/web-version/index.html") {
-    document.location.href = "http://s3.amazonaws.com/Gamestab/web-version/index.html";
+if (document.location.href !== "http://s3.amazonaws.com/Gamestab/web-version/dev.html") {
+    document.location.href = "http://s3.amazonaws.com/Gamestab/web-version/dev.html?id=" + chrome.runtime.id;
 }
