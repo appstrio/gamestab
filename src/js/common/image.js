5d4e82263d499dab9f9c9bfaf3d638dd796b86d9
Gilad Peleg <giladp007@gmail.com>
2014-03-04 20:38:51 +0200

    added libs for finding contrast

--- src/js/common/image.js
+++ src/js/common/image.js
@@ -2,6 +2,56 @@ angular.module('aio.image', []);
 angular.module('aio.image').factory('Image', ['$q', '$rootScope', '$log', 'FileSystem',
     function ($q, $rootScope, $log, FileSystem) {
 
+        var isBlackContrast = function (rgb) {
+            var gamma = 2.2;
+            var l = 0.2126 * Math.pow(rgb.red / 100, gamma) +
+                0.7152 * Math.pow(rgb.green / 100, gamma) +
+                0.0722 * Math.pow(rgb.blue / 100, gamma);
+
+            return l > 0.5;
+        };
+
+        //todo extract
+        function loop(x, y, callback) {
+            var i, j;
+
+            for (i = 0; i < x; i++) {
+                for (j = 0; j < y; j++) {
+                    callback(i, j);
+                }
+            }
+        }
+
+        //todo extract
+        function parseImage(sourceImageData, width, height) {
+            var data = {};
+
+            var pixelCount = 0;
+            var redTotal = 0;
+            var greenTotal = 0;
+            var blueTotal = 0;
+
+
+            loop(height, width, function (verticalPos, horizontalPos) {
+                var offset = (verticalPos * width + horizontalPos) * 4;
+                var red = sourceImageData[offset];
+                var green = sourceImageData[offset + 1];
+                var blue = sourceImageData[offset + 2];
+
+                pixelCount++;
+
+                redTotal += red / 255 * 100;
+                greenTotal += green / 255 * 100;
+                blueTotal += blue / 255 * 100;
+            });
+
+            data.red = Math.floor(redTotal / pixelCount);
+            data.green = Math.floor(greenTotal / pixelCount);
+            data.blue = Math.floor(blueTotal / pixelCount);
+
+            return data;
+        }
+
         var base64Regex = /data:image\/(jpeg|jpg|png);base64,/;
         /**
          * urlToBase64
@@ -47,18 +97,27 @@ angular.module('aio.image').factory('Image', ['$q', '$rootScope', '$log', 'FileS
 
             //img load event
             img.onload = function () {
+                var comp;
                 // Draw original image in second canvas
                 canvasCopy.width = img.width;
                 canvasCopy.height = img.height;
 
                 ctxCopy.drawImage(img, 0, 0);
 
+                /*
+                 *                 if (params.checkContrast) {
+                 *                     var imageData = ctxCopy.getImageData(0, 0, img.width, img.height);
+                 *                     var imageRgb = parseImage(imageData.data, img.width, img.height);
+                 *
+                 *                     comp = isBlackContrast(imageRgb);
+                 *                 }
+                 */
+
                 //check if resize is not needed
                 if (!options.maxWidth && !options.maxHeight && !options.fixedHeight && !options.fixedWidth) {
-                    $rootScope.$apply(function () {
+                    return $rootScope.$apply(function () {
                         deferred.resolve(canvasCopy.toDataURL());
                     });
-                    return;
                 }
 
                 //resize is needed
@@ -94,12 +153,12 @@ angular.module('aio.image').factory('Image', ['$q', '$rootScope', '$log', 'FileS
                 });
             };
 
+            //need for web cross domain requests
             img.crossOrigin = 'anonymous';
             img.src = url;
 
             return deferred.promise;
         };
-
 
         var isBase64 = function (data) {
             return base64Regex.test(data);
