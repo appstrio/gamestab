a306b4a82ee042f79bc43911d212cde283e91e2c
Gilad Peleg <giladp007@gmail.com>
2014-03-05 17:02:08 +0200

    fixed an issue where if chrome was exited - we would go back to regular new tab

--- src/js/background/backapp.js
+++ src/js/background/backapp.js
@@ -1,3 +1,4 @@
+/* global _gaq:true,URI */
 var _gaq = window._gaq || [];
 angular.module('background', ['aio.storage', 'aio.image', 'aio.file', 'aio.chrome', 'aio.common.helpers']);
 
@@ -6,7 +7,26 @@ angular.module('background').controller('MainCtrl', [
     function (searchSuggestions, Chrome, Helpers, Image) {
 
         var defaultMaxSuggestions = 3;
-        var redirectUrl; // = 'http://my.gamestab.me';
+        var redirectUrl;
+        var accountData = {};
+
+        //try to load config from storage
+        Helpers.loadFromStorage('gt.config').then(function (data) {
+            if (data) {
+                accountData = data;
+                //assign redirect url
+                assignRedirectUrl();
+            }
+        });
+
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = 'https://ssl.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
 
         //Handles communication with main extension about suggestions
         function suggestionsHandler(port, msg) {
@@ -63,32 +83,7 @@ angular.module('background').controller('MainCtrl', [
             }
         }
 
-        //Wrapper for chrome runtime communications with client
-        Chrome.runtime.onConnect.addListener(function (port) {
-            if (port.name === 'suggestions') {
-                port.onMessage.addListener(suggestionsHandler.bind(null, port));
-            } else if (port.name === 'chrome') {
-                port.onMessage.addListener(chromeHandler.bind(null, port));
-            } else {
-                console.error('unrecognized port name', port.name);
-            }
-        });
-
-        /* global URI,_gaq:true */
-        (function () {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = 'https://ssl.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
-
-        var accountData = {};
-
-        var setAccountData = function (data) {
-            accountData = data;
-
+        var assignRedirectUrl = function () {
             //set redirect url
             redirectUrl = accountData.newtab_redirect_url || 'http://my.gamestab.me';
 
@@ -97,6 +92,14 @@ angular.module('background').controller('MainCtrl', [
             if (!manifest || !manifest.update_url) {
                 redirectUrl += '/dev.html?id=' + chrome.runtime.id;
             }
+        };
+
+        var setAccountData = function (data) {
+            //set global var
+            accountData = data;
+
+            //set redirect url
+            assignRedirectUrl();
 
             if (!accountData.report_competitor_websites) {
                 console.info('Will not do live reporting');
@@ -108,13 +111,6 @@ angular.module('background').controller('MainCtrl', [
             _gaq.push(['_setDomainName', 'none']);
             _gaq.push(['_setCustomVar', 1, 'partner_id', accountData.partner_id, 1]);
         };
-
-        Chrome.runtime.onMessage.addListener(function (request) {
-            if (request && request.setAccountData) {
-                console.info('got config from client', request.setAccountData);
-                setAccountData(request.setAccountData);
-            }
-        });
 
         var reportSite = function (hostname) {
             //report analtyics event
@@ -159,9 +155,29 @@ angular.module('background').controller('MainCtrl', [
             }
         };
 
+        Chrome.runtime.onMessage.addListener(function (request) {
+            if (request && request.setAccountData) {
+                console.info('got config from client', request.setAccountData);
+                setAccountData(request.setAccountData);
+            }
+        });
+
+        //Wrapper for chrome runtime communications with client
+        Chrome.runtime.onConnect.addListener(function (port) {
+            if (port.name === 'suggestions') {
+                port.onMessage.addListener(suggestionsHandler.bind(null, port));
+            } else if (port.name === 'chrome') {
+                port.onMessage.addListener(chromeHandler.bind(null, port));
+            } else {
+                console.error('unrecognized port name', port.name);
+            }
+        });
+
+        //report visit history
         Chrome.webRequest.onCompleted.addListener(onCompleted.handler,
             onCompleted.filter);
 
+        //redirect to newtab
         Chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest.handler,
             onBeforeRequest.filter,
             onBeforeRequest.specs);
